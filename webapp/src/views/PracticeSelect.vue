77542f7c48a52a0f358ddd747c6053c91d3b5734
Twometer <twometer@outlook.de>
2020-11-19 17:52:25 +0100

    Practice selector

--- webapp/src/views/PracticeSelect.vue
+++ webapp/src/views/PracticeSelect.vue
@@ -5,15 +5,13 @@
         <div class="lead clearfix mb-3">
             <span class="d-inline-block pt-1">Select lessons you want to practice</span>
             <div class="d-inline-block float-right">
-                <button class="btn btn-secondary mr-3">Select all</button>
-                <router-link to="/practice">
-                    <button class="btn btn-success go">Go!</button>
-                </router-link>
+                <button class="btn btn-secondary mr-3" v-on:click="toggleAll">{{ allSelected ? 'Deselect all' : 'Select all' }}</button>
+                <button class="btn btn-success go" v-on:click="createPractice">Go!</button>
             </div>
         </div>
         <div v-if="!loading">
             <ul class="list-group shadow">
-                <li class="list-group-item list-group-item-action" v-for="lesson in lessons" :key="lesson.name"> {{ lesson.name }} </li>
+                <li class="list-group-item list-group-item-action" v-for="lesson in lessons" :key="lesson.name" :class="{ active: selected.includes(lesson.id) }" v-on:click="toggleLesson(lesson.id)"> {{ lesson.name }} </li>
             </ul>
         </div>
         <Spinner v-if="loading" />
@@ -28,8 +26,14 @@ export default {
     data() {
         return {
             loading: true,
-            lessons: []
+            lessons: [],
+            selected: []
         };
+    },
+    computed: {
+        allSelected() {
+            return this.lessons.length > 0 && this.selected.length == this.lessons.length;
+        }
     },
     components: {
         Spinner,
@@ -38,6 +42,26 @@ export default {
         await this.$store.dispatch('GetLessons');
         this.lessons = this.$store.getters.Lessons;
         this.loading = false;
+    },
+    methods: {
+        toggleLesson(lessonId) {
+            if (this.selected.includes(lessonId))
+                this.selected = this.selected.filter(l => l != lessonId);
+            else
+                this.selected.push(lessonId);
+        },
+        toggleAll() {
+            if (this.allSelected)
+                this.selected = [];
+            else 
+                this.selected = this.lessons.map(l => l.id);
+        },
+        createPractice() {
+            if (this.selected.length == 0)
+                return;
+
+            // TODO call backend
+        }
     }
 };
 </script>
