8f05d21e748ce0673073a9cf217e447c97e6ed35
Twometer <twometer@outlook.de>
2020-11-22 02:48:44 +0100

    Inform user about error on practice creation

--- webapp/src/views/PracticeSelect.vue
+++ webapp/src/views/PracticeSelect.vue
@@ -2,6 +2,9 @@
     <div class="practice-select">
         <h1>Practice</h1>
         <hr />
+        <div class="alert alert-danger" v-if="error">
+            <strong>Error: </strong>Could not create practice from empty lesson(s)
+        </div>
         <div class="lead clearfix mb-3">
             <span class="d-inline-block pt-1">Select lessons you want to practice</span>
             <div class="d-inline-block float-right">
@@ -27,7 +30,8 @@ export default {
         return {
             loading: true,
             lessons: [],
-            selected: []
+            selected: [],
+            error: false
         };
     },
     computed: {
@@ -48,23 +52,32 @@ export default {
     },
     methods: {
         toggleLesson(lessonId) {
+            this.error = false;
             if (this.selected.includes(lessonId))
                 this.selected = this.selected.filter(l => l != lessonId);
             else
                 this.selected.push(lessonId);
         },
         toggleAll() {
+            this.error = false;
             if (this.allSelected)
                 this.selected = [];
             else 
                 this.selected = this.lessons.map(l => l.id);
         },
         async createPractice() {
+            this.error = false;
             if (this.selected.length == 0)
                 return;
 
-            await this.$store.dispatch('NewPractice', { target: 'lesson', lessons: this.selected })
-            this.$router.push('/practice');
+            try {
+                await this.$store.dispatch('NewPractice', { target: 'lesson', lessons: this.selected })
+                this.$router.push('/practice');
+            } catch (e) {
+                if (e.response.status == 404) {
+                    this.error = true;
+                }
+            }
         }
     }
 };
